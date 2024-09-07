85c0b252cb01b1b41ac18487e520856749c05925
Mfon Eti-mfon <mfonetimfon@gmail.com>
2024-09-08 00:07:48 +0100

    Generate response from the GPT-4o using simple prompt

--- src/run.py
+++ src/run.py
@@ -1,9 +1,18 @@
 import os
-from openai import OpenAI
 
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-print(client.api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "python quiz. 5 questions."}],
+    max_tokens=256,
+)
+
+breakpoint()
+
+# print(response.choices[0].message.content)
